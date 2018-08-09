a80474764a4ddceb6a3468dcabcf211d84196aa4
Estudiantes <caimanc08@gmail.com>
2018-08-09 07:45:18 -0500

    ejercicios

---
+++ Holamundo/src/holamundo/Holamundo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package holamundo;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class Holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("ingrese nombre");
+        String nombre=sc.next();
+        System.out.println("hola mundo");
+        
+        // TODO code application logic here
+    }
+    
+}
